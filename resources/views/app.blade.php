b7f9919131eb6d1ebdba6cad6dbd31beae3765c5
Bernard Franco <francobernardm@gmail.com>
2026-03-19 15:57:23 +0800

    Isang edit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- sjdfbhjkommmmmmmmmmmmmmmmkdsbfjk safsdfsdfdrf -->
+
+
+<!-- isang edit lang to!! -->
         {{-- Inline script to detect syssdffffffffffffffffffffffftem dark mode preference and apply it immediately --}}
         <script>
             (function() {
